361c2691994aa5f5a6d80a6a0b024b0f425846f5
UndeadZeratul <undeadZeratul@gmail.com>
2015-09-05 14:01:12 -0500

    Disable Hiding of Potions in NEI
    They each have their own effect, might as well keep them displayed.

--- config/INpureProjects/custom_nei_filters/vanilla.js
+++ config/INpureProjects/custom_nei_filters/vanilla.js
@@ -4,6 +4,6 @@ if (vanilla_enabled){
         // Vanilla items and blocks have special handlers.
 	    NEI.hide("minecraft", block);
     }
-	NEI.override("minecraft:potion", [0]);
+	// NEI.override("minecraft:potion", [0]);
 	NEI.override("minecraft:spawn_egg", [0]);
 }
