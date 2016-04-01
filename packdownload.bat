76e1e3f19c9251ca7653044db6194d228c8702fa
UndeadZeratul <undeadZeratul@gmail.com>
2016-04-01 18:58:13 -0500

    Update Bunches of Mods
    Hopefully everything's in its rightful place...

--- packdownload.bat
+++ packdownload.bat
@@ -5,16 +5,13 @@ del /s /q "common\base\mods\*.jar"
 del /s /q "common\prod\mods\*.jar"
 del /s /q "client\dev\mods\*.jar"
 del /s /q "client\base\mods\*.jar"
-::del /s /q "client\prod\mods\*.jar"
-::del /s /q "server\dev\mods\*.jar"
-::del /s /q "server\base\mods\*.jar"
-::del /s /q "server\prod\mods\*.jar"
+del /s /q "client\prod\mods\*.jar"
+del /s /q "server\dev\mods\*.jar"
+del /s /q "server\base\mods\*.jar"
+del /s /q "server\prod\mods\*.jar"
 
 :: Set ModpackDownloader Variable
 for /f "delims=" %%i IN ('dir *.jar /b') DO set modpackdownloader=%%i
-
-:: Download Forge
-java -jar "%modpackdownloader%" common/base/forge.json common/base/loaders
 
 :: Download All Common Mods
 java -jar "%modpackdownloader%" common/dev/mods.json common/dev/mods
@@ -24,9 +21,9 @@ java -jar "%modpackdownloader%" common/prod/mods.json common/prod/mods
 :: Download All Client Mods
 java -jar "%modpackdownloader%" client/dev/mods.json client/dev/mods
 java -jar "%modpackdownloader%" client/base/mods.json client/base/mods
-::java -jar "%modpackdownloader%" client/prod/mods.json client/prod/mods
+java -jar "%modpackdownloader%" client/prod/mods.json client/prod/mods
 
 :: Download All Server Mods
-::java -jar "%modpackdownloader%" server/dev/mods.json server/dev/mods
+java -jar "%modpackdownloader%" server/dev/mods.json server/dev/mods
 java -jar "%modpackdownloader%" server/base/mods.json server/base/mods
-::java -jar "%modpackdownloader%" server/prod/mods.json server/prod/mods
+java -jar "%modpackdownloader%" server/prod/mods.json server/prod/mods
--- packupdate.bat
+++ packupdate.bat
@@ -10,16 +10,16 @@ for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d ModPackDownloader*.jar') do @
 for /f "delims=" %%i IN ('dir *.jar /b') DO set modpackdownloader=%%i
 
 :: Update All Common Mod JSONs
-::java -jar "%modpackdownloader%" common/dev/mods.json common/dev/mods -updateMods -mcVersion=1.7.10 -releaseType=release
-java -jar "%modpackdownloader%" common/base/mods.json common/base/mods -updateMods -mcVersion=1.7.10 -releaseType=release
-java -jar "%modpackdownloader%" common/prod/mods.json common/prod/mods -updateMods -mcVersion=1.7.10 -releaseType=release
+java -jar "%modpackdownloader%" common/dev/mods.json common/dev/mods -updateMods -releaseType=release
+java -jar "%modpackdownloader%" common/base/mods.json common/base/mods -updateMods -releaseType=release
+java -jar "%modpackdownloader%" common/prod/mods.json common/prod/mods -updateMods -releaseType=release
 
 :: Update All Client Mod JSONs
-java -jar "%modpackdownloader%" client/dev/mods.json client/dev/mods -updateMods -mcVersion=1.7.10 -releaseType=release
-java -jar "%modpackdownloader%" client/base/mods.json client/base/mods -updateMods -mcVersion=1.7.10 -releaseType=release
-::java -jar "%modpackdownloader%" client/prod/mods.json client/prod/mods -updateMods -mcVersion=1.7.10 -releaseType=release
+java -jar "%modpackdownloader%" client/dev/mods.json client/dev/mods -updateMods -releaseType=release
+java -jar "%modpackdownloader%" client/base/mods.json client/base/mods -updateMods -releaseType=release
+java -jar "%modpackdownloader%" client/prod/mods.json client/prod/mods -updateMods -releaseType=release
 
 :: Update All Server Mod JSONs
-::java -jar "%modpackdownloader%" server/dev/mods.json server/dev/mods -updateMods -mcVersion=1.7.10 -releaseType=release
-::java -jar "%modpackdownloader%" server/base/mods.json server/base/mods -updateMods -mcVersion=1.7.10 -releaseType=release
-::java -jar "%modpackdownloader%" server/prod/mods.json server/prod/mods -updateMods -mcVersion=1.7.10 -releaseType=release
+java -jar "%modpackdownloader%" server/dev/mods.json server/dev/mods -updateMods -releaseType=release
+java -jar "%modpackdownloader%" server/base/mods.json server/base/mods -updateMods -releaseType=release
+java -jar "%modpackdownloader%" server/prod/mods.json server/prod/mods -updateMods -releaseType=release
