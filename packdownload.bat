afed8020bde2358f8c3f8b6c6c30791197442025
UndeadZeratul <undeadZeratul@gmail.com>
2017-05-09 21:32:11 -0500

    Comment out missing MPD JSONs
    They're causing issues on Jenkins

--- packdownload.bat
+++ packdownload.bat
@@ -16,14 +16,14 @@ for /f "delims=" %%i IN ('dir *.jar /b') DO set modpackdownloader=%%i
 :: Download All Common Mods
 java -jar "%modpackdownloader%" -manifest common/dev/mods.json  -folder common/dev/mods
 java -jar "%modpackdownloader%" -manifest common/base/mods.json -folder common/base/mods
-java -jar "%modpackdownloader%" -manifest common/prod/mods.json -folder common/prod/mods
+::java -jar "%modpackdownloader%" -manifest common/prod/mods.json -folder common/prod/mods
 
 :: Download All Client Mods
-java -jar "%modpackdownloader%" -manifest client/dev/mods.json  -folder client/dev/mods
+::java -jar "%modpackdownloader%" -manifest client/dev/mods.json  -folder client/dev/mods
 java -jar "%modpackdownloader%" -manifest client/base/mods.json -folder client/base/mods
-java -jar "%modpackdownloader%" -manifest client/prod/mods.json -folder client/prod/mods
+::java -jar "%modpackdownloader%" -manifest client/prod/mods.json -folder client/prod/mods
 
 :: Download All Server Mods
-java -jar "%modpackdownloader%" -manifest server/dev/mods.json  -folder server/dev/mods
+::java -jar "%modpackdownloader%" -manifest server/dev/mods.json  -folder server/dev/mods
 java -jar "%modpackdownloader%" -manifest server/base/mods.json -folder server/base/mods
-java -jar "%modpackdownloader%" -manifest server/prod/mods.json -folder server/prod/mods
+::java -jar "%modpackdownloader%" -manifest server/prod/mods.json -folder server/prod/mods
--- packupdate.bat
+++ packupdate.bat
@@ -12,14 +12,14 @@ for /f "delims=" %%i IN ('dir *.jar /b') DO set modpackdownloader=%%i
 :: Update All Common Mod JSONs
 java -jar "%modpackdownloader%" -manifest common/dev/mods.json  -folder common/dev/mods  -updateMods -releaseType release
 java -jar "%modpackdownloader%" -manifest common/base/mods.json -folder common/base/mods -updateMods -releaseType release
-java -jar "%modpackdownloader%" -manifest common/prod/mods.json -folder common/prod/mods -updateMods -releaseType release
+::java -jar "%modpackdownloader%" -manifest common/prod/mods.json -folder common/prod/mods -updateMods -releaseType release
 
 :: Update All Client Mod JSONs
-java -jar "%modpackdownloader%" -manifest client/dev/mods.json  -folder client/dev/mods  -updateMods -releaseType release
+::java -jar "%modpackdownloader%" -manifest client/dev/mods.json  -folder client/dev/mods  -updateMods -releaseType release
 java -jar "%modpackdownloader%" -manifest client/base/mods.json -folder client/base/mods -updateMods -releaseType release
-java -jar "%modpackdownloader%" -manifest client/prod/mods.json -folder client/prod/mods -updateMods -releaseType release
+::java -jar "%modpackdownloader%" -manifest client/prod/mods.json -folder client/prod/mods -updateMods -releaseType release
 
 :: Update All Server Mod JSONs
-java -jar "%modpackdownloader%" -manifest server/dev/mods.json  -folder server/dev/mods  -updateMods -releaseType release
+::java -jar "%modpackdownloader%" -manifest server/dev/mods.json  -folder server/dev/mods  -updateMods -releaseType release
 java -jar "%modpackdownloader%" -manifest server/base/mods.json -folder server/base/mods -updateMods -releaseType release
-java -jar "%modpackdownloader%" -manifest server/prod/mods.json -folder server/prod/mods -updateMods -releaseType release
+::java -jar "%modpackdownloader%" -manifest server/prod/mods.json -folder server/prod/mods -updateMods -releaseType release
